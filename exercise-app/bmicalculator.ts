f2d80cbb5fcc9d2c52c8b83d161ccc1a51cc5be4
Petra <petra.paloniemi94@hotmail.com>
2025-09-19 13:23:38 +0300

    9.5 WebBMI

--- exercise-app/bmicalculator.ts
+++ exercise-app/bmicalculator.ts
@@ -30,15 +30,17 @@ const calculateBmi = (height: number, weight: number) => {
   }
 };
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something bad happened.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
 
 export default calculateBmi;
--- exercise-app/index.ts
+++ exercise-app/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
+import calculateBmi from "./bmicalculator";
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = req.query.height;
+  const weight = req.query.weight;
+
+  if (Number(height) && Number(weight)) {
+    const bmiRange = calculateBmi(Number(height), Number(weight));
+    res.json({ weight: weight, height: height, bmi: bmiRange });
+  } else {
+    res.json({ error: "malformatted parameters" });
+  }
 });
 
 const PORT = 3003;
